98172977ddea1f48a81cf9899b165e6b95a635cd
lucasgalvao111 <lgsantos2312@gmail.com>
2025-10-30 13:31:26 -0300

    Conjunto de atualizações

--- core/settings.py
+++ core/settings.py
@@ -73,11 +73,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'jacare2025',
+        'HOST': 'db.blrdmsxtwxkxjoeaipja.supabase.co',
+        'PORT': '5432',
+    },
 }
 
 
