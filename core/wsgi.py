71a80a509630a6fef6ea4658707c777680aca944
Rodrigo Abreu <143100178+rodrigoabreuuu@users.noreply.github.com>
2025-11-04 19:13:41 -0300

    Update wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -2,11 +2,11 @@ import os
 import sys
 
 # Caminho até a pasta do projeto
-path = '/home/tertiusupx5/Tertius'
+path = '/home/tertiusupx5/TertiusUPX5'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Tertius.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
